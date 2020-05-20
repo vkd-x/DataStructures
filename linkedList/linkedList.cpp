a32af8b21388cb82fdef8acd50ee48f4dd1323e8
Vineet Dwivedi <vkd.x2021@gmail.com>
2020-05-20 11:56:40 +0530

    Initial adding Linked List template version

---
+++ linkedList/linkedList.cpp
@@ -0,0 +1,28 @@
+#include "linkedlist.h"
+
+int main()
+{
+    //Create new Linked List
+    LinkedList<int> ll;
+    ll.push_back(10);
+    ll.push_back(20);
+    ll.push_back(30);
+    ll.push_back(40);
+    ll.push_back(50);
+    ll.push_front(60);
+    ll.push_front(70);
+    ll.printList();
+
+    LinkedList<std::string> llStr;
+
+    llStr.push_back("I");
+    llStr.push_back("just");
+    llStr.push_back("made");
+    llStr.push_back("my");
+    llStr.push_back("templated");
+    llStr.push_back("linkedList");
+    llStr.push_back("of");
+    llStr.push_back("String");
+    llStr.push_front("Hello! ");
+    llStr.printList();
+}
---
+++ linkedList/linkedlist.h
@@ -0,0 +1,86 @@
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
+
+#include <iostream>
+
+using namespace std;
+
+template <class T>
+class LinkedList
+{
+    //Single node
+    struct Node
+    {
+        T elem;
+        Node *next;
+    };
+    //Head Pointer of Linked List
+    Node* head;
+
+public:
+    //Constructor
+    LinkedList(){head = nullptr;}
+    //Destructor
+    ~LinkedList()
+    {
+        Node* tmp;
+        while(head)
+        {
+            tmp = head;
+            head = head->next;
+            delete tmp;
+        }
+    }
+    void push_back(T elem);
+    Node* newNode(T elem); //Returns pointer to new node
+    void push_front( T elem); //Add node to the front of Linked List
+    void printList();
+};
+
+template <class T>
+void LinkedList<T>::push_back( T elem)
+{
+    auto tmp = head;
+    if(head == nullptr)
+        head = newNode(elem);
+    else
+    {
+        while (tmp->next != nullptr) {
+            tmp = tmp->next;
+        }
+        tmp->next = newNode(elem);
+    }
+}
+
+template <class T>
+typename LinkedList<T>::Node* LinkedList<T>::newNode(T elem)
+{
+    Node* node = new Node;
+    node->elem = elem;
+    node->next = nullptr;
+    return node;
+}
+
+template <class T>
+void LinkedList<T>::push_front(T elem)
+{
+    Node* tmp = newNode(elem);
+    tmp->next = head;
+    head = tmp;
+}
+
+template <class T>
+void LinkedList<T>::printList()
+{
+    auto tmp = head;
+    while(tmp != nullptr)
+    {
+        std::cout << tmp->elem << " ";
+        tmp = tmp->next;
+    }
+    std::cout << std::endl;
+}
+
+
+#endif // LINKEDLIST_H
