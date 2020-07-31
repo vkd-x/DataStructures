53b85012554f58e12434ab71ca6163e2f06a5220
Vineet Dwivedi <vkd.x2021@gmail.com>
2020-07-31 20:02:50 +0530

    Basic Templated Binary Tree implmentation

--- binaryTree/binaryTree.cpp
+++ binaryTree/binaryTree.cpp
@@ -1,29 +1,29 @@
 #include "binaryTree.h"
 
-
-template <class T>
-BinaryTree::addNode(T elem)
-{
-}
-
-BinaryTree::deleteNode()
-{}
-
-BinaryTree::search()
-{}
-
-BinaryTree::inOrderTraversal()
-{}
-
-BinaryTree::preOrderTraversal()
-{}
-
-BinaryTree::postOrderTraversal()
-{}
-
-
 int main()
 {
+	std::cout <<" Calling main " << std::endl;
+	BinaryTree<int> bTree;
 
+	bTree.insertNode(50);
+	bTree.insertNode(60);
+	bTree.insertNode(10);
+	bTree.insertNode(20);
+	bTree.insertNode(30);
+	bTree.insertNode(40);
+	bTree.insertNode(70);
+	bTree.insertNode(80);
+	bTree.insertNode(90);
+
+	std::cout << std::endl << " Inorder..." << std::endl;
+	bTree.inOrder();
+	std::cout << std::endl << " Inorder exit " << std::endl;
+	std::cout << std::endl << " Preorder..." << std::endl;
+	bTree.preOrder();
+	std::cout << std::endl << " Preorder exit " << std::endl;
+	std::cout << std::endl << " Postorder..." << std::endl;
+	bTree.postOrder();
+	std::cout << std::endl << " Postorder exit " << std::endl;
+	std::cout << std::endl;
+	std::cout << "Exiting main.." << std::endl;
 }
-
--- binaryTree/binaryTree.h
+++ binaryTree/binaryTree.h
@@ -1,20 +1,119 @@
+/*
+*
+* Author - Vineet Dwivedi 
+* C++ - Implemention of Templated Binary Tree 
+*
+*/
 
+#ifndef BINARY_TREE_H
+#define BINARY_TREE_H
 
+#include <iostream>
 
+using namespace std;
+
+template <class T>
 class BinaryTree
 {
-    template <class T>
+private:
     struct Node
     {
-        T val;
-        Node *rght;
-        Node *left;
+        Node* left;
+        Node* rght;
+        T elem;
+        Node() = delete; //Dont create instance without elem
+        Node(T elem): elem(elem),left(nullptr),rght(nullptr){}
     };
+
+    //Only member variable
+    Node* rootNode;
+
 public:
-    void addNode();
-    void deleteNode();
-    void search();
-    void inOrderTraversal();
-    void preOrderTraversal();
-    void postOrderTraversal();
+    //Constructor
+    BinaryTree(){rootNode = nullptr;}
+    void insertNode(T elem);
+    void inOrder();
+    void preOrder();
+    void postOrder();
+
+private:
+    void insertNodeRecur(Node*& , T elem);
+    void inOrderRecur(Node* );
+    void preOrderRecur(Node* );
+    void postOrderRecur(Node* );
+    Node* newNode(T elem){Node* node = new Node(elem); return node;}
 };
+
+template <class T>
+void BinaryTree<T>::insertNode(T elem)
+{
+    insertNodeRecur(rootNode, elem);
+}
+
+template <class T>
+void BinaryTree<T>::insertNodeRecur(Node*& node, T elem)
+{
+    if(node == nullptr){node = newNode(elem); return;}
+    else if(node->elem > elem){insertNodeRecur(node->left, elem);}
+    else{insertNodeRecur(node->rght, elem);}
+}
+
+
+/*In order recursive implementation*/
+template <class T>
+void BinaryTree<T>::inOrder()
+{
+    inOrderRecur(rootNode);
+}
+
+template <class T>
+void BinaryTree<T>::inOrderRecur(Node* node)
+{
+    if(node == nullptr) return;
+    else
+    {
+        inOrderRecur(node->left);
+        std::cout << node->elem <<" ";
+        inOrderRecur(node->rght);
+    }
+}
+
+/*Pre order recursive implementation*/
+template <class T>
+void BinaryTree<T>::preOrder()
+{
+    preOrderRecur(rootNode);
+}
+
+template <class T>
+void BinaryTree<T>::preOrderRecur(Node* node)
+{
+    if(node == nullptr) return;
+    else
+    {
+        std::cout << node->elem <<" ";
+        preOrderRecur(node->left);
+        preOrderRecur(node->rght);
+    }
+}
+
+
+/*Post order recursive implementation*/
+template <class T>
+void BinaryTree<T>::postOrder()
+{
+    postOrderRecur(rootNode);
+}
+
+template <class T>
+void BinaryTree<T>::postOrderRecur(Node* node)
+{
+    if(node == nullptr) return;
+    else
+    {
+        postOrderRecur(node->left);
+        postOrderRecur(node->rght);
+        std::cout << node->elem <<" ";
+    }
+}
+#endif
